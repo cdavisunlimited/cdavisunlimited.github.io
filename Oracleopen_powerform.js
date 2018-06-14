eb022dc0ca8e1ffb698fb2b15f3a5b824750c96b
Swarlos Estavez <cdavisunlimited@gmail.com>
2018-06-14 10:07:18 -0700

    Add files via upload

---
+++ Oracleopen_powerform.js
@@ -0,0 +1,45 @@
+function $(elem) {return document.getElementById(elem);} //simple id reference
+
+number_messages = 0;
+var last_id     = "";
+
+function check_messages() { //check for messages from iframe
+    if (location.hash != last_id) {
+        last_id = location.hash;
+        number_messages++;
+        last_id = last_id.replace("_"," ");
+        last_id = last_id.substr(1);
+        last_id = last_id.substr(0,last_id.indexOf("&"));
+        var message_color;
+        var extra_text;
+        if(last_id=="Signing Complete" || last_id=="Viewing Complete"){
+            message_color="green";
+            extra_text = "";
+        }else{
+            message_color="red";
+            extra_text = "<p style='text-align:center'><button type='button' onclick='window.location=\"embedded.html\"' style='display:inline;'>Reload form?</button></p>";
+        }
+        document.getElementById("powerform").innerHTML = "<center><h3 style='color:"+message_color+";border:none;font-size:20px;text-align:center;'>" + last_id + "</h3><br/>"+extra_text+"</center>";
+    }
+}
+
+function checkURL() {
+    if(document.getElementById('document').contentWindow.location.href == "http://www.docusign.com") {// if the location of the iframe is the unique url
+        window.location = "http://www.google.com"; //redirect this page somewhere else.
+		}
+}
+
+
+function open_embeddedform(form) {
+
+  
+   var form_url = "https://demo.docusign.net/Member/PowerFormSigning.aspx?PowerFormId=de5bbec1-7fbf-46a9-8053-7fb8cdcbf428";
+    form_url += "&Customer_UserName=" + $("name").value;
+    form_url += "&Customer_Email=" + $("email").value;
+	form_url += "&Phone=" + $("phone").value;
+	
+
+$("powerform").innerHTML = '<iframe id="document" onload="checkURL();" src="' + form_url + '" border="0"></iframe>';
+    //$("powerform").innerHTML = '<iframe id="document" src="' + form_url + '" border="0"></iframe>';
+}
+setInterval(check_messages, 200);
